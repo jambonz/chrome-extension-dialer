225a5d16b06acaa060714c5082e968a8e21112b5
Quan HL <quan.luuhoang8@gmail.com>
2024-05-07 13:29:47 +0700

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -3,34 +3,34 @@ const HTMLPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = [
-  {
-    entry: "./src/content/index.tsx",
-    target: "web",
-    mode: "production",
-    output: {
-      path: path.join(__dirname, "dist"),
-      filename: "js/content.js",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: [
-            {
-              loader: "ts-loader",
-              options: {
-                compilerOptions: { noEmit: false },
-              },
-            },
-          ],
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".ts", ".tsx", ".js"],
-    },
-  },
+  // {
+  //   entry: "./src/content/index.tsx",
+  //   target: "web",
+  //   mode: "production",
+  //   output: {
+  //     path: path.join(__dirname, "dist"),
+  //     filename: "js/content.js",
+  //   },
+  //   module: {
+  //     rules: [
+  //       {
+  //         test: /\.tsx?$/,
+  //         use: [
+  //           {
+  //             loader: "ts-loader",
+  //             options: {
+  //               compilerOptions: { noEmit: false },
+  //             },
+  //           },
+  //         ],
+  //         exclude: /node_modules/,
+  //       },
+  //     ],
+  //   },
+  //   resolve: {
+  //     extensions: [".ts", ".tsx", ".js"],
+  //   },
+  // },
   {
     entry: "./src/background/index.ts",
     target: "web",
@@ -57,68 +57,6 @@ module.exports = [
     },
     resolve: {
       extensions: [".ts", ".tsx", ".js"],
-    },
-  },
-  {
-    entry: {
-      index: "./src/index.tsx",
-    },
-    mode: "production",
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: [
-            {
-              loader: "ts-loader",
-              options: {
-                compilerOptions: { noEmit: false },
-              },
-            },
-          ],
-          exclude: /node_modules/,
-        },
-        {
-          exclude: /node_modules/,
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            "style-loader",
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
-          ],
-        },
-        {
-          test: /\.txt$/i,
-          type: "asset/source",
-        },
-      ],
-    },
-    plugins: [
-      new CopyPlugin({
-        patterns: [{ from: "public", to: ".." }],
-      }),
-      ...getHtmlPlugins(["index"]),
-    ],
-    resolve: {
-      alias: {
-        src: path.resolve(__dirname, "src/"),
-      },
-      extensions: [".tsx", ".ts", ".js", ".txt"],
-    },
-    output: {
-      path: path.join(__dirname, "dist/js"),
-      filename: "[name].js",
-    },
-    performance: {
-      maxEntrypointSize: 512000,
-      maxAssetSize: 512000,
     },
   },
   {
@@ -162,7 +100,12 @@ module.exports = [
         },
       ],
     },
-    plugins: [...getHtmlPlugins(["index"])],
+    plugins: [
+      new CopyPlugin({
+        patterns: [{ from: "public", to: ".." }],
+      }),
+      ...getHtmlPlugins(["index"]),
+    ],
     resolve: {
       alias: {
         src: path.resolve(__dirname, "src/"),
